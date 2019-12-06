5e09fecb76cc71551aee6eb3eaeb0d3c8cebfef3
Nick Carter <56028215+nickcarter123@users.noreply.github.com>
2019-12-06 16:25:14 +0100

    Add files via upload

---
+++ Fraction.cpp
@@ -0,0 +1,36 @@
+#include "stdafx.h"
+#include <iostream>
+
+
+
+class Fraction
+{
+private:
+	int m_numerator;
+	int m_denominator;
+
+public:
+	Fraction()
+	{
+		m_numerator = 5;
+		m_denominator = 2;
+	}
+
+	int getNumerator() { return m_numerator; }
+	void setNumerator(int numerator) { m_numerator = numerator; }
+	int getDenominator() { return m_denominator; }
+	void setDenominator(int denominator) { m_denominator = denominator; }
+	double getValue() { return static_cast<double>(m_numerator) / m_denominator; }
+};
+
+int main()
+{
+	Fraction drob;
+	drob.setNumerator(9);
+	drob.setDenominator(8);
+	std::cout << drob.getNumerator() << "/" << drob.getDenominator() << "=" << drob.getNumerator() / drob.getDenominator() << '\n';
+	std::cout << drob.getValue();
+
+	return 0;
+}
+
